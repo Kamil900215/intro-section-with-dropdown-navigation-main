b73e99d2781506d95ecaed84d1f5495d1b93a5c6
Kamil Matyszczak <kamil>
2022-12-10 17:38:49 +0100

    before cleanup and code review

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 document.addEventListener('click', e => {
-
-   const isHamburger = e.target.matches('[data-hamburger]')
-  
-   if(!isHamburger && e.target.closest('[data-navigation]') != null) return;
-
+   
    let selectedDropdown
+   const isHamburger = e.target.matches('[data-hamburger]')
+   const isFeatures = e.target.matches('[data-button-feat]')
+   const isCompany = e.target.matches('[data-button-company]')
+   
+   if(isFeatures){
+      e.target.classList.toggle('active')
+   }
+   if(isCompany){
+      e.target.classList.toggle('active')
+   }
+   
+   
+  
+   
+   
+   if(!isHamburger && e.target.closest('[data-navigation]') != null) return;
    if(isHamburger){
       selectedDropdown = e.target.closest('[data-navigation]')
       selectedDropdown.classList.toggle('active')
    }
+
+   
    document.querySelectorAll('[data-navigation].active').forEach(dropdown => {
       if(dropdown === selectedDropdown) return
       dropdown.classList.remove('active')
